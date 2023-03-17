463f212038bcdb9832b3c73e52c2261728c31d97
Taiwo <taibann00@gmail.com>
2023-03-17 13:00:18 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints allsingle digit numbers of
+ * base 10 from 0
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d", a);
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
+
