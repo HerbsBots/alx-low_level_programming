864ce315083d4983a022ceb9c5509bcea90a1263
Taiwo <taibann00@gmail.com>
2023-04-03 12:35:43 -0700

    memset

--- 0x07-pointers_arrays_strings/_putchar.c
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -1,17 +1,13 @@
- /* including the header file (unistd.h) for the function (write) */
+#include <unistd.h>
 
-	#include <unistd.h>
-	/**
-	 * _putchar - writing the character c to stdout
-	 * @c: The character that prints
-	 *
-	 * when successful (1) return (1)
-	 * if error occurs, (negative of 1, i.e -1) is returned, and the error number is 	   also set apropraitely.
-	 */
-	int _putchar(char c)
-	
-	{
-		/* calling the "write" function to write the character to stdout */
-		return (write(1, &c, 1));
-
-	}
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
