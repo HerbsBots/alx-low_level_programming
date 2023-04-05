2a1294c88e9ab28f7afc16835d9601fa091e35cf
Taiwo <taibann00@gmail.com>
2023-04-05 11:42:01 -0700

    Why is it so important to dream? Because, in my dreams we are together

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _prints_rev_recursion - prints s tring in reverse
- * @s: the string that is to be printed
+ * _print_rev_recursion - prints a string in reverse
+ * @s: the string to be printed
  */
 
 void _print_rev_recursion(char *s)
@@ -13,4 +13,3 @@ _print_rev_recursion(s + 1);
 _putchar(*s);
 }
 }
-
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -1,2 +1,2 @@
-She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget
+She locked away a secret, deep inside herself, something she once knew to be true... butchose to forget
 Why is it so important to dream? Because, in my dreams we are together
