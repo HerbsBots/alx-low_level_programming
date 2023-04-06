1c17d14e802f24da817ab46b2cdfb8cdb670e592
Taiwo <taibann00@gmail.com>
2023-04-05 17:42:13 -0700

    They say we only use a fraction of our brain's true potential. Now that's when we're awake. When we're asleep, we can do almost anything

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,56 +1,49 @@
 #include "main.h"
 
-int find_strlen(char *s);
-int check_palindrome(char *s, int len, int index);
-int is_palindrome(char *s);
+int check_palindrome(char *s, int index, int len)
+int _strlen_recursion(char *s)
 
 /**
- * find_strlen - Returns the length of a string
- * @s: the string to be measured
- * Return: the length of the string
- */
-
-int find_strlen(char *s)
-{
-int len = 0;
-if (*(s + len))
-{
-len++;
-len += find_strlen(s + len);
-}
-return (len);
-}
-
-/**
- * check_palindrome - checks if a string is a palindrome
- * @s: the string to be checked
- * @len: the length of s
- * @index: the index of the string to be checked
- * Return: if the string is a palindrome - 1
- * if the string is not a palindrome - 0
- */
-
-int check_palindrome(char *s, int len, int index)
-{
-if (s[index] == s[len / 2])
-return (1);
-if (s[index] == s[len - index - 1])
-return (check_palindrome(s, len, index + 1));
-return (0);
-}
-
-/**
- * is_palindrome - checks if a string is a palindrome
- * @s: the string to be checked
- * Return: if the string is a palindrome - 1
- * if the string is not a palindrome - 0
+ * is_palindrome: to check if a string is palindrome
+ * @s: the string to reverse
+ *
+ * Return: 1 if it is, and 0 if it is not
  */
 
 int is_palindrome(char *s)
 {
-int index = 0;
-int len + find_strlen(s);
-if (!(*s))
+if (*s == 0)
 return (1);
-return (check_palindrome(s, len, index));
+return (check_palindrome(s, 0, _strlen_recursion(s)));
+}
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to be calculated for
+ *
+ * Return: length of the string
+ */
+
+int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+return (0);
+return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * check_palindrome - checks the cahr recursively for palindrome
+ * @s: string for checking
+ * @i: iterator
+ * @len: length of the string
+ *
+ * Return: 1 if palindrome, 0 if not
+ */
+
+int check_palindrome(char *s, int len, int i)
+if (*(s + i) != *(s + len - 1))
+return (0);
+if (i >= len)
+return (1);
+return (check_palindrome(s, i + 1, len - 1));
 }
