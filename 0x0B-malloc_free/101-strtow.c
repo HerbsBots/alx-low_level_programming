f1f78ed35f1dcedddf5f6194906685ea2dcb5a7b
Taiwo <taibann00@gmail.com>
2023-04-11 20:20:55 -0700

    I will show you how great I am

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,59 +1,79 @@
 #include "main.h"
 #include <stdlib.h>
-
-void util(char **, char *);
-void create_word(char **, char *, int, int, int);
-
+int word_len(char *str);
+int count_words(char *str);
+char **strtow(char *str);
 /**
- * strtow - splits a string into words
- * @str: the string
- *
- * Return:returns a pointer to an array of strings (words)
+ * word_len - locates the index marking the end of the first word contained in a string
+ * @str: the string to be searched
+ * Return: the index marking the end of the initial word pointed to by str
  */
-
-char **strtow(char *str)
+int word_len(char *str)
 {
-int i, flag, len;
-char **words;
-if (str == NULL || str[0] == '\0' || (str[0] == ' ' && str[1] == '\0'))
-return (NULL);
-i = flag = len = 0;
-while (str[i])
+int index = 0, len = 0;
+while (*(str + index) && *(str + index) != ' ')
 {
-if (flag == 0 && str[i] != ' ')
-flag = 1;
-if (i > 0 && str[i] == ' ' && str[i - 1] != ' ')
-{
-flag = 0;
 len++;
+index++;
 }
-i++;
+return (len);
 }
-len += flag == 1 ? 1 : 0;
-if (len == 0)
-return (NULL);
-words = (char **)malloc(sizeof(char *) * (len + 1));
-if (words == NULL)
-return (NULL);
-util(words, str);
-words[len] = NULL;
+/**
+ * count_words - counts the number of words contained in a string
+ * @str: the string to be searched
+ * Return: the number of words contained within a string
+ */
+int count_words(char *str)
+{
+int index = 0, words = 0, len = 0;
+for (index = 0; *(str + index); index++)
+len++;
+for (index = 0; index < len; index++)
+{
+if (*(str + index) != ' ')
+{
+words++;
+index += word_len(str + index);
+}
+}
 return (words);
 }
-
 /**
- * util - a util function for fetching words into an array
- * @words: the strings array
- * @str: the string
+ * strtow - splits a string into words
+ * @str: the string to be split
+ * Return: if str = NULL, str= "", or the function fails - NULL
+ * otherwise - a pointer to an array of strings (words).
  */
-
-void util(char **words, char *str)
+char **strtow(char *str)
 {
-int i, j, start, flag;
-i = j = flag = 0;
-while (str[i])
+char **strings;
+int index = 0, words, w, letters, l;
+if (str == NULL || str[0] == '\0')
+return (NULL);
+words = count_words(str);
+if (words == 0)
+return (NULL);
+strings = malloc(sizeof(char *) * (words + 1));
+if (strings == NULL)
+return (NULL);
+for (w = 0; w < words; w++)
 {
-if (flag == 0 && str[i] != ' ')
+while (str[index] == ' ')
+index++;
+letters = word_len(str + index);
+strings[w] = malloc(sizeof(char) * (letters + 1));
+if (strings[w] == NULL)
 {
-start = i;
-flag = 1;
+for (; w >= 0; w--)
+free(strings[w]);
+free(strings);
+return (NULL);
 }
+for (l = 0; l < letters; l++)
+strings[w][l] = str[index++];
+strings[w][l] = '\0';
+}
+strings[w] = NULL;
+return (strings);
+}
+
