809644d45e3b3151e461d5f93c8ea40919e98a09
Taiwo <taibann00@gmail.com>
2023-04-13 12:29:39 -0700

    _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,16 +11,14 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-void *mem;
-char *filler;
-unsigned int index;
+char *a;
+unsigned int b;
 if (nmemb == 0 || size == 0)
-return (NULL);
-mem = malloc(size * nmemb);
-if (mem == NULL)
 return (NULL)
-filler = mem;
-for (index = 0; index < (size * nmemb); index++)
-filler[index] = '\0';
-return (mem);
+a = malloc(nmemb * size);
+if (a == NULL)
+return (NULL);
+for (b = 0; b < (nmemb * size); b++)
+a[b] = 0;
+return (a);
 }
