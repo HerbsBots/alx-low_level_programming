28abecd0fd21a88f517bc1ca82a8ad355cacde9c
Taiwo <taibann00@gmail.com>
2023-04-17 11:23:45 -0700

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file of the program
+ * Return: always = 0
+ */
+
+int main(void)
+{
+printf("%s\n", _FILE_);
+return (0);
+}
+
--- 0x0D-preprocessor/README.md
+++ 0x0D-preprocessor/README.md
@@ -1,2 +1,3 @@
 0x0D. C - Preprocessor
 Pi
+File name
