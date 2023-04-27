3e49a2aadc9c8530c89fab4dec341f429bfa61bb
HerbsBots <taibann00@gmail.com>
2023-04-27 15:18:43 -0700

    Print list

--- 0x12-singly_linked_lists/list.h
+++ 0x12-singly_linked_lists/list.h
@@ -1,16 +1,18 @@
-#ifndef _LISTS_
-#define _LISTS_
+#ifndef LISTS_H
+#define LISTS_H
 
-#include <stdio.h>
+/*
+ * Desc: header file containing prototypes and definitions
+ * for all functions and types written in this project
+ */
+
 #include <stdlib.h>
-#include <string.h>
 
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
- *
  * Description: singly linked list node structure
  * for Holberton project
  */
@@ -22,6 +24,7 @@ unsigned int len;
 struct list_s *next;
 } list_t;
 
+int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
@@ -29,4 +32,3 @@ list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
 #endif
-
