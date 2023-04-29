4e17d8eaf02c0f8074d1e22d5320cbfe2606d743
HerbsBots <taibann00@gmail.com>
2023-04-29 10:03:15 -0700

    Add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,32 +3,25 @@
 /**
  * add_node - add new node at the beginning of list_t list
  *
- * @head: points to a structure
+ * @head: head of the linked list
  *
- * @str: string to a structure
+ * @str: string to store in the list
  * Return: the address of a new element
+ *
  */
 
 list_t *add_node(list_t **head, const char *str)
 {
-list_t *newNode;
-unsigned int length = 0;
-newNode = malloc(sizeof(list_t));
-if (newNode == NULL)
-{
-free(newNode);
+list_t *new;
+size_t nchar;
+new = malloc(sizeof(list_t));
+if (new == NULL)
 return (NULL);
-}
-newNode->str = strdup(str);
-while (str[length] != '\0')
-{
-length++;
-}
-newNode->len = length;
-if (*head != NULL)
-newNode->next = *head;
-if (*head == NULL)
-newNode->next = NULL;
-*head = newNode;
+new->str = strdup(str);
+for (nchar = 0; str[nchar]; nchar++)
+;
+new->len = nchar;
+new->next = *head;
+*head = new;
 return (*head);
 }
