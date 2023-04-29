a224b261a155393c9f4f6dedff5707a2d43e65d4
HerbsBots <taibann00@gmail.com>
2023-04-29 10:22:05 -0700

    Add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,13 +1,12 @@
 #include "lists.h"
 
 /**
- * add_node - add new node at the beginning of list_t list
+ * add_node - adds a new node at the beginning of a list_t list
  *
  * @head: head of the linked list
  *
  * @str: string to store in the list
- * Return: the address of a new element
- *
+ * Return: address of the head
  */
 
 list_t *add_node(list_t **head, const char *str)
@@ -21,7 +20,7 @@ new->str = strdup(str);
 for (nchar = 0; str[nchar]; nchar++)
 ;
 new->len = nchar;
-new->next = *head;
+new->next = *ahead;
 *head = new;
 return (*head);
 }
