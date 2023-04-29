9fab758a74bc216741722b6fcd4d1531c1ad3ae4
HerbsBots <taibann00@gmail.com>
2023-04-29 09:52:31 -0700

    Add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,36 +1,34 @@
 #include "lists.h"
-#include <string.h>
 
 /**
- * add_node - addition of a new node to the list_t list at the beginning
+ * add_node - add new node at the beginning of list_t list
  *
- * @head: a pointer that points to the head of the list_t list
+ * @head: points to a structure
  *
- * @str: the string that gets added to the list_t list
- *
- *
- * Return: NULL on failure and other wise address of a new element
+ * @str: string to a structure
+ * Return: the address of a new element
  */
 
 list_t *add_node(list_t **head, const char *str)
 {
-list_t *new;
-char *dup_str;
-int len;
-new = malloc(sizeof(list_t));
-if (new == NULL)
-return (NULL);
-dup_str = strdup(str);
-if (dup_str == NULL)
+list_t *newNode;
+unsigned int length = 0;
+newNode = malloc(sizeof(list_t));
+if (newNode == NULL)
 {
-free(new);
+free(newNode);
 return (NULL);
 }
-for (len = 0; str[len];)
-len++;
-new->str = dup_str;
-new->len = len;
-new->next = *head;
-*head = new;
-return (new);
+newNode->str = strdup(str);
+while (str[length] != '\0')
+{
+length++;
+}
+newNode->len = length;
+if (*head != NULL)
+newNode->next = *head;
+if (*head == NULL)
+newNode->next = NULL;
+*head = newNode;
+return (*head);
 }
