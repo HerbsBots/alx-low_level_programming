c76b14eef0ff7e6027691cc5ab32568ba0186911
HerbsBots <taibann00@gmail.com>
2023-05-01 07:27:53 -0700

    Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,19 +3,21 @@
 
 /**
  * print_listint - prints all the elements of a listint_t list
- * @h: A pointer to the head of the list_t list
  *
- * Return: The number of nodes in the list_t list
+ * @p: a pointer to the head if the list_t list
+ *
+ * Return: the number of nodes in the list_t list
+ *
  */
 
-size_t print_listint(const listint_t *h)
+size_t print_listint(const listint_t *p)
 {
 size_t nodes = 0;
-while (h)
+while (p)
 {
 nodes++;
-printf("%d\n", h->n);
-h = h->next;
+printf("%d\n", p->n);
+p = p->next;
 }
 return (nodes);
 }
