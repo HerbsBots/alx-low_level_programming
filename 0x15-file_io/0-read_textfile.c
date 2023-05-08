85d833253af3767d9c8e02055751fa318bef1c46
HerbsBots <taibann00@gmail.com>
2023-05-08 13:17:35 -0700

    Tread lightly, she is near

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prins the letters
+ *
+ * @filename: filename
+ *
+ * @letters: numbers of ,etters printed
+ *
+ * Return: numbers of letters printed, if fails, return 0
+ *
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+int fd;
+ssize_t nrd, nwr;
+char *buf;
+if (!filename)
+return (0);
+fd = open(filename, O_RDONLY);
+if (fd == -1)
+return (0);
+buf = malloc(sizeof(char) * (letters));
+if (!buf)
+return (0);
+nrd = read(fd, buf, letters);
+nwr = write(STDOUT_FILENO, buf, nrd);
+close(fd);
+free(buf);
+return (nwr);
+}
